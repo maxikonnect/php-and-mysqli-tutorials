35a9dce9ac193b0f9920226ddd2c88708f90ac6f
maxikonnect <tribillio@gmail.com>
2024-12-10 17:19:56 +0000

    php

---
+++ gettypelesson6.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+        <meta name="author" content="Abradu Frimpong">
+        <meta name="description" content="php and mysqli practice exercise">
+        <title>PHP AND MYSQL PRACTICE</title>
+        <link rel="stylesheet" href="./style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    </head>
+    <body>
+        <?php
+            $a = true;
+            $b = 10;
+            $c = "hat";
+
+            echo gettype($a) . "<br>";
+            echo gettype($b) . "<br>";
+            echo gettype($c) . "<br>";
+
+        ?>
+        
+    </body>
+</html>
