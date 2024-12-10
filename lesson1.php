fcd4ef9b36bec4bb9650eb1a849c67500578870f
maxikonnect <tribillio@gmail.com>
2024-12-10 09:58:15 +0000

    lesson1

---
+++ lesson1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+        <meta name="author" content="Abradu Frimpong">
+        <meta name="description" content="php and mysqli practice exercise">
+        <title>PHP AND MYSQL PRACTICE</title>
+    </head>
+    <body>
+        <?php
+            echo "Hi, i am php script";
+
+        ?>
+    </body>
+</html>
